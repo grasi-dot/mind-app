7615bac483d8ff99c6154397dfc664664bfd7a0f
Grasi Eduarda <grasieduarda775@gmail.com>
2022-01-14 17:52:03 -0300

    CRUD to 0

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,7 @@
 class Content < ApplicationRecord
   belongs_to :user
 
-  validates :title, :descrption, presence: true
+  validates :title, :description, presence: true
   has_many :tag_contents
   has_many :tags, through: :tag_contents
 end
